8c40e392464f169d156a18d98b09bbd64c7125e1
sjctyjin <a0979319516@google.com>
2023-10-27 16:34:27 +0800

    TODO-web

--- main.py
+++ main.py
@@ -25,43 +25,60 @@ def home():
 
 @app.route('/Join', methods=['GET'])
 def room():
+    global usernum_list
 
-    return render_template(f'Room.html')
+    if len(usernum_list) == 4:
+        return redirect('/blcok_area')
+    else:
+        return render_template(f'Room.html')
+@app.route('/blcok_area', methods=['GET'])
+def blcok():
+    global usernum_list
 
+    return render_template(f'Block_A.html')
 
 @app.route('/user', methods=['POST'])
 def user():
     global usernum
+    global usernum_list
     data_res = request.get_json()
     print(data_res)
-    if usernum != []:
-        for i in usernum:
-            if i == data_res['user']:
-                usernum.remove(data_res['user'])
-                return Response('you like shit')
-
-    usernum.append(data_res['user'])
-    return Response('you like shit')
+    # if len(usernum_list) != 0:
+    if usernum_list[data_res['user']] == "":
+        usernum_list[data_res['user']] = data_res['selected']
+        return Response('you like shit')
+    else:
+        usernum_list[data_res['user']] = ""
+        return Response('you like shit')
+    # return Response('you like shit')
 #TODO 從querystring中確認 選擇腳色的是哪位玩家，進而紀錄該玩家的操作動作
 #取得玩家人數
 @app.route('/menber', methods=['GET'])
 def menber():
     global usernum
+    global usernum_list
     print(usernum)
-    return jsonify({f"menber":usernum})
+    print(usernum_list)
+    print(len(usernum_list))
+    return jsonify({f"menber":usernum_list,"userlog":usernum})
 #清空人員
 @app.route('/clear_user', methods=['POST'])
 def clear_menber():
     global usernum
+    global usernum_list
     usernum = []
+    usernum_list = []
     return jsonify({f"menber":usernum})
 #獲取使用者最初進入時間
 @app.route('/usertime', methods=['POST'])
 def usertime():
     global usernum_list
+    global usernum
     data_res = request.get_json()
     print(data_res)
     usernum_list[data_res['set_time']] = ""
+    usernum.append(data_res['set_time'])
+
     print(usernum_list)
     return render_template(f'Room.html')
 if __name__ == "__main__":
